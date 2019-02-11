3c51f554c28d6e3a094d2fa5d9a5fd7887fe70a5
jip <dev@guitaresphere.Com>
2019-02-11 22:05:53 +0100

    update sql Base and requete

--- sql-base/ngshop.sql
+++ sql-base/ngshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Lun 11 Février 2019 à 18:50
+-- Généré le :  Lun 11 Février 2019 à 22:04
 -- Version du serveur :  5.7.25-0ubuntu0.18.10.2
 -- Version de PHP :  7.2.10-0ubuntu1
 
@@ -19,32 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `ngshop`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `User`
---
-
-CREATE TABLE `User` (
-  `idUser` int(11) NOT NULL,
-  `Nom` varchar(45) DEFAULT NULL,
-  `Prenom` varchar(45) DEFAULT NULL,
-  `Mail` varchar(45) DEFAULT NULL,
-  `Password` varchar(45) DEFAULT NULL,
-  `pseudo` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `User`
---
-
-INSERT INTO `User` (`idUser`, `Nom`, `Prenom`, `Mail`, `Password`, `pseudo`) VALUES
-(1, 'FAURE', 'JULIEN', 'jf@mail.com', '123456789', 'nautique');
-
-
-
-
 
 -- --------------------------------------------------------
 
@@ -72,31 +46,9 @@ CREATE TABLE `Adress` (
 
 INSERT INTO `Adress` (`idAdress`, `Label`, `Nom`, `Prenom`, `Numero`, `Rue`, `Complement`, `Cp`, `Ville`, `Pays`, `User_id`) VALUES
 (3, 'adresse maison ', 'FAURE', 'JULIEN', '6', 'RUE DES SAULES', '', '26000', 'VALENCE', 'FRANCE', 1),
-(4, 'ADRESSE  PAPA', 'FAURE', 'MAURICE', '52', 'BL DES ANCIENS', '10 ARR', '75000', 'PARIS', 'FRANCE', 1);
-
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Categorie`
---
-
-CREATE TABLE `Categorie` (
-  `idCategorie` int(11) NOT NULL,
-  `Nom` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `Categorie`
---
-
-INSERT INTO `Categorie` (`idCategorie`, `Nom`) VALUES
-(1, 'SECU'),
-(2, 'INF0RMATIQUE');
-
-
-
-
+(4, 'ADRESSE  PAPA', 'FAURE', 'MAURICE', '52', 'BL DES ANCIENS', '10 ARR', '75000', 'PARIS', 'FRANCE', 1),
+(5, 'adresse perso', 'alpha', 'raoul', '47', 'rue des fleurs', '', '38185', 'GRENOBLE', 'FRANCE', 2),
+(6, 'adresse memo', 'laventin', 'sonia', '78', 'av. des pins parasols', '', '75000', 'PARIS', 'FRANCE', 3);
 
 -- --------------------------------------------------------
 
@@ -123,7 +75,41 @@ INSERT INTO `Article` (`idArticle`, `Nom`, `Descripion`, `Poids`, `Prix`, `Stock
 (1, 'App shield power !', 'une application qui vous protèges', '1.00', '10.00', 50, 'assets/icon-shield-orange.svg', 1),
 (2, 'App hub Green', 'Une application qui fait pousser vos plantes', '10.00', '25.00', 50, 'assets/icon-hub-green.svg', 1),
 (3, 'App hub blue', 'Une application qui est bleu', '2.50', '52.00', 40, 'assets/icon-hub-blue.svg', 1),
-(4, 'App Gps gLue', 'une application qui pointe le nord', '40.00', '57.00', 50, 'assets/icon-direction-blue.svg', 1);
+(4, 'App Gps gLue', 'une application qui pointe le nord', '40.00', '57.00', 50, 'assets/icon-direction-blue.svg', 1),
+(5, 'App yellow Rise', 'App qui fait se lever le soleil ! ', '2.00', '27.00', 0, 'assets/icon-hub-yellow.svg', 2),
+(6, 'App recharger', 'app qui recharge  ... des Apps', '5.00', '15.00', 0, 'assets/avatar1.png', 1),
+(7, 'App american', 'app qui se connecte à votre banque', '2.00', '13.50', 85, 'assets/custom4.svg', 2),
+(8, 'Urban qhit', 'App qui fait le ménage à ta place', '0.51', '178.00', 1, 'assets/custom2.svg', 2),
+(15, 'App direction', 'App qui donne la direction', '0.45', '14.50', 100, 'assets/icon-load-Balancing-green.svg', 1),
+(16, 'App Box', 'app qui lie une Boite en carton', '0.50', '14.70', 0, 'assets/icon-Microservices-green.svg', 2),
+(17, 'App secure wallet', 'app qui sécurise votre wallet bitcoin', '0.75', '40.00', 18, 'assets/icon-NGINX-WAF-1.svg', 1),
+(18, 'App Timer', 'App qui donne l\'heure', '1.00', '10.99', 15, 'assets/icon-Web-and-Mobile-Applications-green.svg', 2),
+(19, 'App Who is', 'app qui vous dit qui vous êtes ...', '0.75', '19.99', 178, 'assets/notFound.svg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Categorie`
+--
+
+CREATE TABLE `Categorie` (
+  `idCategorie` int(11) NOT NULL,
+  `Nom` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `Categorie`
+--
+
+INSERT INTO `Categorie` (`idCategorie`, `Nom`) VALUES
+(1, 'SECU'),
+(2, 'INF0RMATIQUE'),
+(3, 'ELECTRONIQUE'),
+(4, 'CONNECTIQUE'),
+(5, 'GAME'),
+(6, 'GESTION'),
+(7, 'NO APPLE'),
+(8, 'MUSIQUE');
 
 -- --------------------------------------------------------
 
@@ -145,7 +131,9 @@ CREATE TABLE `Commande` (
 --
 
 INSERT INTO `Commande` (`idCommande`, `commande_num`, `Date_de_Commande`, `Adress_id_livraison`, `Adress_id_facturation`, `User_id`) VALUES
-(1, 'ED45SR15FR', '2019-02-11 13:30:00', 3, 4, 1);
+(1, 'ED45SR15FR', '2019-02-11 13:30:00', 3, 4, 1),
+(2, 'S4RE5SD4ED', '2019-02-09 08:00:00', 6, 6, 3),
+(3, '4Y789YU5U4', '2019-02-11 17:00:00', 5, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -165,7 +153,10 @@ CREATE TABLE `ligne_cmd` (
 --
 
 INSERT INTO `ligne_cmd` (`Article_id`, `Commande_id`, `ligne_cmd_Qts`, `ligne_cmd_prix`) VALUES
-(1, 1, 5, '10.00');
+(1, 1, 5, '10.00'),
+(8, 2, 1, '178.00'),
+(15, 2, 3, '14.50'),
+(19, 3, 3, '19.99');
 
 -- --------------------------------------------------------
 
@@ -189,6 +180,41 @@ CREATE TABLE `Promo` (
 
 INSERT INTO `Promo` (`idPromo`, `Nom`, `Date_de_Debut`, `Date_de_Fin`, `%_Remise`, `Prix_Remise`, `idArticle`) VALUES
 (1, 'BLACK FRIDAY', '2019-02-21 00:00:00', '2019-02-28 00:00:00', '5.0', NULL, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `User`
+--
+
+CREATE TABLE `User` (
+  `idUser` int(11) NOT NULL,
+  `Nom` varchar(45) DEFAULT NULL,
+  `Prenom` varchar(45) DEFAULT NULL,
+  `Mail` varchar(45) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  `pseudo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `User`
+--
+
+INSERT INTO `User` (`idUser`, `Nom`, `Prenom`, `Mail`, `Password`, `pseudo`) VALUES
+(1, 'FAURE', 'JULIEN', 'jf@mail.com', '123456789', 'nautique'),
+(2, 'ALF', 'marie', 'am@mail.com', '123456789', 'marie666'),
+(3, 'TIBAULT', 'KEVIN', 'tk@mail.com', '123456789', 'elfique5'),
+(4, 'foster', 'kathy', 'jf@mail.com', '123456789', 'nautique'),
+(5, 'Bridden', 'Merlina', 'mbridden0@ocn.ne.jp', 'rr6UGke60K', 'mbridden0'),
+(6, 'Gurney', 'Jammie', 'jgurney1@indiegogo.com', 'g9tZF45vRy', 'jgurney1'),
+(7, 'Elayne', 'Pierri', 'epierri2@jigsy.com', 'RjE32YV', 'epierri2'),
+(8, 'Siemianowicz', 'Alessandro', 'asiemianowicz3@tripod.com', 'Prk5bgu5Rt0', 'asiemianowicz3'),
+(9, 'Mei', 'Trippitt', 'mtrippittt@bizjournals.com', 'uuqMVZ1qgWeY', 'mtrippittt'),
+(10, 'Deeyn', 'Hubane', 'dhubaneu@weather.com', 'nbbtBCEa', 'dhubaneu'),
+(11, 'Erena', 'Tanton', 'etantonv@cnbc.com', 'ZJz1XZnf7', 'etantonv'),
+(12, 'Maxine', 'Oxbie', 'moxbiew@pcworld.com', 'llpkkmzi1nD', 'moxbiew'),
+(13, 'Charmaine', 'Fareweather', 'cfareweatherx@tumblr.com', 'NjVSq27bkC5Y', 'cfareweatherx'),
+(14, 'Eamon', 'Lathwood', 'elathwoody@cdc.gov', '10qUqUBn6oT', 'elathwoody');
 
 --
 -- Index pour les tables exportées
@@ -253,22 +279,22 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT pour la table `Adress`
 --
 ALTER TABLE `Adress`
-  MODIFY `idAdress` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idAdress` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `Article`
 --
 ALTER TABLE `Article`
-  MODIFY `idArticle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idArticle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT pour la table `Categorie`
 --
 ALTER TABLE `Categorie`
-  MODIFY `idCategorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idCategorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT pour la table `Commande`
 --
 ALTER TABLE `Commande`
-  MODIFY `idCommande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idCommande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `Promo`
 --
@@ -278,7 +304,7 @@ ALTER TABLE `Promo`
 -- AUTO_INCREMENT pour la table `User`
 --
 ALTER TABLE `User`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- Contraintes pour les tables exportées
 --
